a46c8b8795710314894bef78bfea860e5b4d1ab7
Anurag Kumar <114430275+Audioslave17@users.noreply.github.com>
2024-07-06 14:11:05 +0530

    xzz

--- todo-example/src/Todo.jsx
+++ todo-example/src/Todo.jsx
@@ -1,23 +1,6 @@
 import { useState } from "react";
 
 function Todo() {
-    // const [tasks, setTasks] = useState(["eat","sleep","concoure","repeat"]);
-    // const [newTask, setNewtask] = useState("");
-    // function handleInputChange(event){
-    //     setNewtask(event.target.value);
-    // }
-    // function addTask(){
-    //     if(newTask.trim() !== ""){
-    //         setTasks(t => [...t,newTask]);
-    //         setNewtask("");
-    //     }
-        
-    // }
-    // function deleteTask(index){
-    //     const update = tasks.filter((_,i) => i!==index);
-    //     setTasks(update);
-    // }
-    // let taskDetail = tasks.map((task,index)=><li key={index}>{task} <button onClick={()=>deleteTask(index)}>delete</button></li>)
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewtask] = useState("");
 
@@ -36,17 +19,6 @@ function Todo() {
     }
     const list = tasks.map((task,index)=><li key={index}><span>{task}</span> <button onClick={()=>deleteTask(index)}>delete</button></li>)
     return(
-        // <div>
-        //     <h1>ToDo-List</h1>
-        //     <div>
-        //         <input type="text" placeholder="Enter your task" value={newTask} onChange={handleInputChange}/>
-        //         <button onClick={addTask}>add</button>
-        //     </div>
-        //     <ol>
-        //         <span>{taskDetail}</span>
-                
-        //     </ol>
-        // </div>
         <div>
             <h1>TODO list</h1>
             <div>
