bf3d13892b334e3b72ffe6119a25c46f15b589da
Anurag Kumar <114430275+Audioslave17@users.noreply.github.com>
2024-07-08 19:22:53 +0530

    two

--- todo-example/src/Todo.jsx
+++ todo-example/src/Todo.jsx
@@ -1,32 +1,29 @@
 import { useState } from "react";
 
-function Todo() {
+function Todo() {what
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewtask] = useState("");
-
     const handleInput = (e) => {
         setNewtask(e.target.value);
     }
-
-    const addTask = () => {
-        setTasks(t => [...t, newTask]);
+    const add = () => {
+        setTasks(t => [...t,newTask]);
         setNewtask("");
     }
-
-    function deleteTask (index) {
-        const update = tasks.filter((_,i) => i!==index);
+    const del = (index) => {
+        const update = tasks.filter((_,i)=> i!==index);
         setTasks(update);
     }
-    const list = tasks.map((task,index)=><li key={index}><span>{task}</span> <button onClick={()=>deleteTask(index)}>delete</button></li>)
+    const items = tasks.map((task,index)=><li key={index}><span>{task}</span> <button onClick={()=>del(index)}>delete</button></li>)
     return(
         <div>
-            <h1>TODO list</h1>
+            <h1>TODO</h1>
             <div>
-                <input type="text" placeholder="Enter the task" value ={newTask} onChange={handleInput}/>
-                <button onClick={addTask}>Add</button>
+                <input type="text" placeholder="Enter the task" value={newTask} onChange={handleInput}/>
+                <button onClick={add}>add</button>
             </div>
             <div>
-                <ol>{list}</ol>
+                <ol>{items}</ol>
             </div>
         </div>
     )
