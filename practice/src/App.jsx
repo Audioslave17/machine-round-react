acd9f299b1b33bf5f02c4b8ab47234769d9b2d16
Anurag Kumar <114430275+Audioslave17@users.noreply.github.com>
2024-07-09 11:19:05 +0530

    abc

---
+++ practice/src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
