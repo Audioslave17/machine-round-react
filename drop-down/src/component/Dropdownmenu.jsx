3cf3fecbb00046fe1ab667728259366bab93c80d
Anurag Kumar <114430275+Audioslave17@users.noreply.github.com>
2024-07-09 19:54:53 +0530

    dropdown

---
+++ drop-down/src/component/Dropdownmenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Dropdownmenu = () => {
+  return (
+    <div className='flex flex-col'>
+        <div className='flex flex-col gap-4'>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </div>
+    </div>
+  )
+}
+
+export default Dropdownmenu
